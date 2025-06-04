e46a808cd0147be40e9461adf4957e823a0cf8fa
Adriano Rotondo <adriano.rotondo.dev@gmail.com>
2025-06-04 17:10:40 +0200

    Fix comment formatting and improve function documentation in script.js

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -1,13 +1,28 @@
-/* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
+/* Scrivi una funzione che accetti un array di nomi e restituisca un nuovo array con le iniziali di ogni parola dell'array fornito */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
-
 // Dichiara la funzione qui.
+//* creo un array vuoto per conservare le iniziali dei nomi
 
+const newArray = [];
+
+/* dichiaro la funzione firstChar dentro la quale inizializzo un ciclo for
+- Ciclo dentro l'array names e recupero la prima lettera [.charAt(0)] di ogni elemento dell'array
+- salvo le lettere selezionate nella variabile firstLetter
+- pusho il contenuto della variabile firstLetter nell'array vuoto newArray creato in precedenza
+*/
+
+function firstChar() {
+  for (let i = 0; i < names.length; i++) {
+    const firstLetter = names[i].charAt(0);
+    newArray.push(firstLetter);
+  }
+  return newArray;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(firstChar(names));
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
